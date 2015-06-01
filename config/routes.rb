c0a295f9bf5f582513a0b1e84f155e73bc910a9b
dkrompic <dkrompic@gmail.com>
2015-06-01 23:20:43 +0200

    Local snippet content execution added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :snippets
   devise_for :users
+
+  # get 'execute/:id' => 'snippets#execute', as: 'execute_snippet'
+  # put 'execute/:id' => 'snippets#execute', as: 'pexecute_snippet'
+  # patch 'execute/:id' => 'snippets#execute', as: 'paexecute_snippet'
+  resources :snippets do
+     member do
+       get :execute
+     end
+  end
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
